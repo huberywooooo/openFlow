4e4cde2c8e88dacae469f62482da6a909c2da624
huberywooooo <hbw8456@163.com>
2024-10-14 14:13:39 +0800

    Create main.m

---
+++ main.m
@@ -0,0 +1,14 @@
+%%%%-----------------------------openFlow--------------------------------------
+%
+%       Description:
+
+%
+%   openFlow: An Open Source Library for Flow behavior of fluid
+%   Author(s): Hubery H.B. Woo (hbw8456@163.com)
+%   Copyright 2023-2024 Chongqing Three Gorges University
+
+
+function main()
+	disp("unfinished.");
+
+end
